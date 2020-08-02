f424bfc6eab0f3834c83d76c532220aa4866482b
Fhinn <68591486+Fhinn@users.noreply.github.com>
2020-08-02 12:53:50 +0200

    Pirate´s Crazy blte and clic ARRR!

---
+++ BONE CRUSHER!/Assets/_Scripts/Target.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.TerrainAPI;
+using Random = UnityEngine.Random;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    private float minForce = 12,
+        maxForce = 16,
+        maxTorque = 10,
+        xRange = 4,
+        ySpawnPos = -6;
+
+    private GameManager gameManager;
+
+    [Range(-100,100)]
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+    public AudioSource audioSource;
+    public AudioClip plop;
+  
+    
+    void Start()
+
+    {
+        audioSource = GetComponent<AudioSource>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(), RandomTorque(),
+            RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+
+        //gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+    
+    /// <summary>
+    /// Genera un vector aleatorio en 3D
+    /// </summary>
+    /// <returns>Fuerza aleatoria para arriba</returns>
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+    
+    /// <summary>
+    /// Genera un número aleatorio
+    /// </summary>
+    /// <returns>Valor aleatorio entre -maxTorque y maxTorque</returns>
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    
+    /// <summary>
+    /// Genera una posición aleatoria
+    /// </summary>
+    /// <returns>Posición aleatoria en 3D, con la coordenada z = 0</returns>
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange,xRange), ySpawnPos);
+    }
+
+    private void OnMouseOver()
+    {
+        if (gameManager.gameState == GameManager.GameState.inGame)
+        {
+            Destroy(gameObject);
+
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+            audioSource.clip = plop;
+            audioSource.Play();
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("killZone"))
+        {
+            Destroy(gameObject);
+            if (gameObject.CompareTag("good"))
+            {
+                gameManager.GameOver();
+            }
+        }
+    }
+}   
---
+++ BONE CRUSHER!/Assets/_Scripts/TengoMiedo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TengoMiedo : MonoBehaviour
+{
+    private Button _button;
+
+    void Start()
+    {
+        _button = GetComponent<Button>();
+        {
+            _button.onClick.AddListener(ByeBye);
+        }
+    }
+
+    void ByeBye()
+    {
+        {
+
+            Application.Quit();
+        }
+    }
+}
+    
+
+
