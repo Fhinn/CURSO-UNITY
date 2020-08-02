3b8574a98529039ac17e969bc39901d905fbb7cc
Fhinn <68591486+Fhinn@users.noreply.github.com>
2020-08-02 12:03:08 +0200

    Atack!
    "Fhinn Solo" use Luke´s XWing to atack the evil Death Star...

---
+++ Fhinn Solo´s Death Star Atack!/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject XWing;
+    public  Vector3 offset = new Vector3(0,13,-18);
+    void Start()
+    {
+    }
+    void Update()
+    {
+        this.transform.position = XWing.transform.position + offset;
+    }
+}
